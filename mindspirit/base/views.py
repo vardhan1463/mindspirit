0fe24bc6ca7684bae9a5dfcf6d065e4435e21a66
Sangati Veera Vardhan <reddyvardhan2003@gmail.com>
2024-04-20 05:30:22 +0530

    secnd_commit

--- mindspirit/base/views.py
+++ mindspirit/base/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [ 
-    {'id':1,'name':'learn pythomn'},
-    {'id':2,'name':'learn shell'},
-    {'id':3,'name':'learn java'},
-]
+# rooms = [ 
+#     {'id':1,'name':'learn pythomn'},
+#     {'id':2,'name':'learn shell'},
+#     {'id':3,'name':'learn java'},
+# ]
 # Create your views here.
 def home(request):
     return HttpResponse('home page')
 
 def homemain(request):
+    rooms=Room.objects.all()
     context = {'rooms': rooms}
     return render(request,'base/home.html', context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room=i
+    room = Room.objects.get(id=pk)
     context={'room':room}
     return render(request,'base/room.html',context)
